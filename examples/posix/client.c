1155e8fa12679eaff2fac6de723c67edf11d2087
Patrick Barrett <patrickbarrett@exosote.com>
2014-12-19 15:43:03 -0600

    Squashed 'picocoap/' content from commit a7e7ed0
    git-subtree-dir: picocoap
    git-subtree-split: a7e7ed0850f46a75fd87c963cf464a7447af1bb6

---
+++ examples/posix/client.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <time.h>
+
+#include "../../src/coap.h"
+ 
+void hex_dump(uint8_t* bytes, size_t len);
+void coap_pretty_print(coap_pdu*);
+
+int main(void)
+{
+  char alias[] = "temp";
+  char cik[] = "a32c85ba9dda45823be416246cf8b433baa068d7";
+
+  char host[] = "coap.exosite.com";
+  char port[] = "5683";
+
+  srand(time(NULL));
+
+  // CoAP Message Setup
+  #define MSG_BUF_LEN 64
+  uint8_t msg_send_buf[MSG_BUF_LEN];
+  coap_pdu msg_send = {msg_send_buf, 0, 64};
+  uint8_t msg_recv_buf[MSG_BUF_LEN];
+  coap_pdu msg_recv = {msg_recv_buf, 0, 64};
+
+  uint16_t message_id_counter = rand();
+
+  // Socket to Exosite
+  int remotesock;
+  size_t bytes_sent;
+  ssize_t bytes_recv;
+  int rv;
+
+  struct addrinfo exohints, *servinfo, *q;
+
+  memset(&exohints, 0, sizeof exohints);
+  exohints.ai_family = AF_UNSPEC;
+  exohints.ai_socktype = SOCK_DGRAM;
+  exohints.ai_flags = AI_PASSIVE;
+
+  if ((rv = getaddrinfo(host, port, &exohints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
+
+
+  // loop through all the results and make a socket
+  for(q = servinfo; q != NULL; q = q->ai_next) {
+    if ((remotesock = socket(q->ai_family, q->ai_socktype, q->ai_protocol)) == -1) {
+      perror("bad socket");
+      continue;
+    }
+
+    break;
+  }
+
+  if (q == NULL) {
+      fprintf(stderr, "Failed to Bind Socket\n");
+      return 2;
+  }
+ 
+  for (;;) 
+  {
+    printf("--------------------------------------------------------------------------------\n");
+
+    // Build Message
+    coap_init_pdu(&msg_send);
+    //memset(msg_send, 0, msg_send_len);
+    coap_set_version(&msg_send, COAP_V1);
+    coap_set_type(&msg_send, CT_CON);
+    coap_set_code(&msg_send, CC_GET); // or POST to write
+    coap_set_mid(&msg_send, message_id_counter++);
+    coap_set_token(&msg_send, rand(), 2);
+    coap_add_option(&msg_send, CON_URI_PATH, (uint8_t*)"1a", 2);
+    coap_add_option(&msg_send, CON_URI_PATH, (uint8_t*)alias, strlen(alias));
+    coap_add_option(&msg_send, CON_URI_QUERY, (uint8_t*)cik, strlen(cik));
+    // to write, set payload:
+    //coap_set_payload(msg_send, &msg_send_len, MSG_BUF_LEN, (uint8_t*)"99", 2);
+
+    // Send Message
+    if ((bytes_sent = sendto(remotesock, msg_send.buf, msg_send.len, 0, q->ai_addr, q->ai_addrlen)) == -1){
+      fprintf(stderr, "Failed to Send Message\n");
+      return 2;
+    }
+
+    printf("Sent.\n");
+    coap_pretty_print(&msg_send);
+
+    // Wait for Response
+    bytes_recv = recvfrom(remotesock, (void *)msg_recv.buf, msg_recv.max, 0, q->ai_addr, &q->ai_addrlen);
+    if (bytes_recv < 0) {
+      fprintf(stderr, "%s\n", strerror(errno));
+      exit(EXIT_FAILURE);
+    }
+
+    msg_recv.len = bytes_recv;
+
+    if(coap_validate_pkt(&msg_recv) == CE_NONE)
+    {
+      printf("Got Valid CoAP Packet\n");
+      if(coap_get_mid(&msg_recv) == coap_get_mid(&msg_send) &&
+         coap_get_token(&msg_recv) == coap_get_token(&msg_send))
+      {
+        printf("Is Response to Last Message\n");
+        coap_pretty_print(&msg_recv);
+      }
+    }else{
+      printf("Received %zi Bytes, Not Valid CoAP\n", msg_recv.len);
+      hex_dump(msg_recv.buf, msg_recv.len);
+    }
+
+    sleep(1); // One Second
+  }
+}
+
+void hex_dump(uint8_t* bytes, size_t len)
+{
+  size_t i, j;
+  for (i = 0; i < len; i+=16){
+    printf("  0x%.3zx    ", i);
+    for (j = 0; j < 16; j++){
+      if (i+j < len)
+        printf("%02hhx ", bytes[i+j]);
+      else
+        printf("%s ", "--");
+    }
+    printf("   %.*s\n", (int)(16 > len-i ? len-i : 16), bytes+i);
+  }
+}
+
+void coap_pretty_print(coap_pdu *pdu)
+{
+  coap_option opt;
+
+  opt.num = 0;
+
+  if (coap_validate_pkt(pdu) == CE_NONE){
+      printf(" ------ Valid CoAP Packet (%zi) ------ \n", pdu->len);
+      printf("Type:  %i\n",coap_get_type(pdu));
+      printf("Code:  %i.%02i\n", coap_get_code_class(pdu), coap_get_code_detail(pdu));
+      printf("MID:   0x%X\n", coap_get_mid(pdu));
+      printf("Token: 0x%llX\n", coap_get_token(pdu));
+
+      while ((opt = coap_get_option(pdu, &opt)).num != 0){
+        if (opt.num == 0)
+          break;
+
+        printf("Option: %i\n", opt.num);
+        if (opt.val != NULL && opt.len != 0)
+          printf(" Value: %.*s (%zi)\n", (int)opt.len, opt.val, opt.len);
+      }
+      // Note: get_option returns payload pointer when it finds the payload marker
+      if (opt.val != NULL && opt.len != 0)
+        printf("Payload: %.*s (%zi)\n", (int)opt.len, opt.val, opt.len);
+    } else {
+      printf(" ------ Non-CoAP Message (%zi) ------ \n", pdu->len);
+      hex_dump(pdu->buf, pdu->len);
+    }
+}
---
+++ tests/coap_test.c
@@ -0,0 +1,424 @@
+#include <stdio.h>
+#include <string.h>
+#include "minunit.h"
+#include "../src/coap.h"
+
+int tests_run = 0;
+
+
+void coap_pretty_print(coap_pdu *pdu);
+void hex_dump(char* bytes, size_t len);
+
+static char * test_math() {
+	mu_assert("[ERROR] 2+2 != 4", 2+2 == 4);
+	return 0;
+}
+
+static char * test_msg_empty_con_getters() {
+	uint8_t ref_bin[] = {64,0,0,0};
+	coap_pdu msg_ref = {ref_bin, 4, 4};
+
+	mu_assert("[ERROR] Empty CON failed validation.",
+	          coap_validate_pkt(&msg_ref) == CE_NONE);
+
+	mu_assert("[ERROR] Empty CON version decoded wrong.",
+	          coap_get_version(&msg_ref) == COAP_V1);
+
+	mu_assert("[ERROR] Empty CON type decoded wrong.",
+	          coap_get_type(&msg_ref) == CT_CON);
+
+	mu_assert("[ERROR] Empty CON tkl decoded wrong.",
+	          coap_get_tkl(&msg_ref) == 0);
+
+	mu_assert("[ERROR] Empty CON token decoded wrong.",
+	          coap_get_token(&msg_ref) == 0);
+
+	mu_assert("[ERROR] Empty CON code decoded wrong.",
+	          coap_get_code(&msg_ref) == CC_EMPTY);
+
+	mu_assert("[ERROR] Empty CON code class decoded wrong.",
+	          coap_get_code_class(&msg_ref) == 0);
+
+	mu_assert("[ERROR] Empty CON code detail decoded wrong.",
+	          coap_get_code_detail(&msg_ref) == 0);
+
+	return 0;
+}
+
+static char * test_msg_empty_con_getters_with_token() {
+	uint8_t ref_bin[] = {66,0,0,0,37,42};
+	coap_pdu msg_ref = {ref_bin, 6, 6};
+
+	mu_assert("[ERROR] Empty CON with token failed validation.",
+	          coap_validate_pkt(&msg_ref) == CE_NONE);
+
+	mu_assert("[ERROR] Empty CON with token version decoded wrong.",
+	          coap_get_version(&msg_ref) == COAP_V1);
+
+	mu_assert("[ERROR] Empty CON with token type decoded wrong.",
+	          coap_get_type(&msg_ref) == CT_CON);
+
+	mu_assert("[ERROR] Empty CON with token tkl decoded wrong.",
+	          coap_get_tkl(&msg_ref) == 2);
+
+	mu_assert("[ERROR] Empty CON with token token decoded wrong.",
+	          coap_get_token(&msg_ref) == 0x2A25);
+
+	mu_assert("[ERROR] Empty CON with token code decoded wrong.",
+	          coap_get_code(&msg_ref) == CC_EMPTY);
+
+	mu_assert("[ERROR] Empty CON with token code class decoded wrong.",
+	          coap_get_code_class(&msg_ref) == 0);
+
+	mu_assert("[ERROR] Empty CON with token code detail decoded wrong.",
+	          coap_get_code_detail(&msg_ref) == 0);
+
+	return 0;
+}
+
+static char * test_msg_empty_con_setters() {
+	uint8_t ref_bin[] = {64,0,0,0};
+	uint8_t test_bin[4];
+	coap_pdu msg_ref = {ref_bin, 4, 4};
+	coap_pdu msg_test = {test_bin, 0, 4};
+
+	mu_assert("[ERROR] Empty CON failed to init.",
+	          coap_init_pdu(&msg_test) == CE_NONE);
+
+	mu_assert("[ERROR] Empty CON failed to set version.",
+	          coap_set_version(&msg_test, COAP_V1) >= 0);
+
+	mu_assert("[ERROR] Empty CON failed to set type.",
+	          coap_set_type(&msg_test, CT_CON) >= 0);
+
+	mu_assert("[ERROR] Empty CON failed to set code.",
+	          coap_set_code(&msg_test, CC_EMPTY) >= 0);
+
+	mu_assert("[ERROR] Empty CON failed to set message ID.",
+	          coap_set_mid(&msg_test, 0) >= 0);
+
+	mu_assert("[ERROR] Empty CON failed to set token.",
+	          coap_set_token(&msg_test, 0, 0) == CE_NONE);
+
+	mu_assert("[ERROR] Empty CON length set wrong.",
+	          msg_test.len == msg_ref.len);
+
+	mu_assert("[ERROR] Empty CON failed to encode.",
+	          memcmp(msg_test.buf, msg_ref.buf, msg_ref.len) == 0);
+
+	return 0;
+}
+
+static char * test_msg_get_con_getters() {
+	uint8_t ref_bin[] = {0x40,0x01,0x00,0x37,0xb2,0x31,0x61,0x04,0x74,0x65,
+	                     0x6d,0x70,0x4d,0x1b,0x61,0x33,0x32,0x63,0x38,0x35,
+	                     0x62,0x61,0x39,0x64,0x64,0x61,0x34,0x35,0x38,0x32,
+	                     0x33,0x62,0x65,0x34,0x31,0x36,0x32,0x34,0x36,0x63,
+	                     0x66,0x38,0x62,0x34,0x33,0x33,0x62,0x61,0x61,0x30,
+	                     0x36,0x38,0x64,0x37};
+	coap_pdu msg_ref = {ref_bin, 54, 54};
+	coap_option option;
+
+	mu_assert("[ERROR] GET CON failed validation.",
+	          coap_validate_pkt(&msg_ref) == CE_NONE);
+
+	mu_assert("[ERROR] GET CON version decoded wrong.",
+	          coap_get_version(&msg_ref) == COAP_V1);
+
+	mu_assert("[ERROR] GET CON type decoded wrong.",
+	          coap_get_type(&msg_ref) == CT_CON);
+
+	mu_assert("[ERROR] GET CON code decoded wrong.",
+	          coap_get_code(&msg_ref) == CC_GET);
+
+	mu_assert("[ERROR] GET CON code class decoded wrong.",
+	          coap_get_code_class(&msg_ref) == 0);
+
+	mu_assert("[ERROR] GET CON code detail decoded wrong.",
+	          coap_get_code_detail(&msg_ref) == 1);
+
+	option = coap_get_option(&msg_ref, NULL);
+	mu_assert("[ERROR] GET CON option zero length was wrong.",
+	          option.len == 2);
+	mu_assert("[ERROR] GET CON option zero number was wrong.",
+	          option.num == CON_URI_PATH);
+	mu_assert("[ERROR] GET CON option zero value was wrong.",
+	          memcmp(option.val, ref_bin+5, option.len) == 0);
+
+	option = coap_get_option(&msg_ref, &option);
+	mu_assert("[ERROR] GET CON option one length was wrong.",
+	          option.len == 4);
+	mu_assert("[ERROR] GET CON option one number was wrong.",
+	          option.num == CON_URI_PATH);
+	mu_assert("[ERROR] GET CON option one value was wrong.",
+	          memcmp(option.val, ref_bin+8, option.len) == 0);
+
+	option = coap_get_option(&msg_ref, &option);
+	mu_assert("[ERROR] GET CON option two length was wrong.",
+	          option.len == 40);
+	mu_assert("[ERROR] GET CON option two number was wrong.",
+	          option.num == CON_URI_QUERY);
+	mu_assert("[ERROR] GET CON option two value was wrong.",
+	          memcmp(option.val, ref_bin+14, option.len) == 0);
+
+	return 0;
+}
+
+static char * test_msg_get_con_setters() {
+	uint8_t ref_bin[] = {0x40,0x01,0x00,0x37,0xb2,0x31,0x61,0x04,0x74,0x65,
+	                     0x6d,0x70,0x4d,0x1b,0x61,0x33,0x32,0x63,0x38,0x35,
+	                     0x62,0x61,0x39,0x64,0x64,0x61,0x34,0x35,0x38,0x32,
+	                     0x33,0x62,0x65,0x34,0x31,0x36,0x32,0x34,0x36,0x63,
+	                     0x66,0x38,0x62,0x34,0x33,0x33,0x62,0x61,0x61,0x30,
+	                     0x36,0x38,0x64,0x37};
+	coap_pdu msg_ref = {ref_bin, 54, 54};
+
+	uint8_t test_bin[54];
+	coap_pdu msg_test = {test_bin, 0,54};
+
+	mu_assert("[ERROR] Empty CON failed to init.",
+	          coap_init_pdu(&msg_test) == CE_NONE);
+
+	mu_assert("[ERROR] GET CON failed to set version.",
+	          coap_set_version(&msg_test, COAP_V1) >= 0);
+
+	mu_assert("[ERROR] GET CON failed to set type.",
+	          coap_set_type(&msg_test, CT_CON) >= 0);
+
+	mu_assert("[ERROR] GET CON failed to set code.",
+	          coap_set_code(&msg_test, CC_GET) >= 0);
+
+	mu_assert("[ERROR] GET CON failed to set message ID.",
+	          coap_set_mid(&msg_test, 0x37) >= 0);
+
+	mu_assert("[ERROR] GET CON failed to set token.",
+	          coap_set_token(&msg_test, 0, 0) >= 0);
+
+	mu_assert("[ERROR] GET CON failed to add first path option.",
+	          coap_add_option(&msg_test, CON_URI_PATH, ref_bin+5, 2) == CE_NONE);
+
+	mu_assert("[ERROR] GET CON failed to add second path option.",
+	          coap_add_option(&msg_test, CON_URI_PATH, ref_bin+8, 4) == CE_NONE);
+
+	mu_assert("[ERROR] GET CON failed to add query option.",
+	          coap_add_option(&msg_test, CON_URI_QUERY, ref_bin+14, 40) == CE_NONE);
+
+	mu_assert("[ERROR] GET CON length set wrong.",
+	          msg_test.len == 54);
+
+	mu_assert("[ERROR] GET CON failed to encode.",
+	          memcmp(msg_ref.buf, msg_test.buf, 54) == 0);
+
+	return 0;
+}
+
+static char * test_msg_get_con_setters_out_order() {
+	uint8_t ref_bin[] = {0x44,0x01,0x00,0x37,0xff,0xff,0xff,0xff,0xb2,0x31,
+		                 0x61,0x04,0x74,0x65,0x6d,0x70,0x4d,0x1b,0x61,0x33,
+		                 0x32,0x63,0x38,0x35,0x62,0x61,0x39,0x64,0x64,0x61,
+		                 0x34,0x35,0x38,0x32,0x33,0x62,0x65,0x34,0x31,0x36,
+		                 0x32,0x34,0x36,0x63,0x66,0x38,0x62,0x34,0x33,0x33,
+		                 0x62,0x61,0x61,0x30,0x36,0x38,0x64,0x37};
+	coap_pdu msg_ref = {ref_bin, 58, 58};
+
+	uint8_t test_bin[64];
+	coap_pdu msg_test = {test_bin, 0, 64};
+
+	//memset(test_bin, 0, 64);
+
+	mu_assert("[ERROR] Empty CON failed to init.",
+	          coap_init_pdu(&msg_test) == CE_NONE);
+
+	mu_assert("[ERROR] GET CON failed to set version. (Out of Order)",
+	          coap_set_version(&msg_test, COAP_V1) >= 0);
+
+	mu_assert("[ERROR] GET CON failed to set type. (Out of Order)",
+	          coap_set_type(&msg_test, CT_CON) >= 0);
+
+	mu_assert("[ERROR] GET CON failed to set code. (Out of Order)",
+	          coap_set_code(&msg_test, CC_GET) >= 0);
+
+	mu_assert("[ERROR] GET CON failed to set message ID. (Out of Order)",
+	          coap_set_mid(&msg_test, 0x37) >= 0);
+
+	mu_assert("[ERROR] GET CON failed to set token. (Out of Order)",
+	          coap_set_token(&msg_test, 0xFFFFFFFF, 4) >= 0);
+
+	mu_assert("[ERROR] GET CON failed to add first path option. (Out of Order)",
+	          coap_add_option(&msg_test, CON_URI_PATH, ref_bin+9, 2) == CE_NONE);
+
+	mu_assert("[ERROR] GET CON failed to add second path option. (Out of Order)",
+	          coap_add_option(&msg_test, CON_URI_PATH, ref_bin+12, 4) == CE_NONE);
+
+	mu_assert("[ERROR] GET CON failed to add query option. (Out of Order)",
+	          coap_add_option(&msg_test, CON_URI_QUERY, ref_bin+18, 40) == CE_NONE);
+
+	mu_assert("[ERROR] GET CON length set wrong. (Out of Order)",
+	          msg_test.len == 58);
+
+	mu_assert("[ERROR] GET CON failed to encode. (Out of Order)",
+	          memcmp(msg_test.buf, msg_ref.buf, 58) == 0);
+
+	return 0;
+}
+
+static char * test_msg_post_con_setters() {
+	uint8_t ref_bin[] = {0x40,0x02,0x00,0x37,0xb2,0x31,0x61,0x04,0x74,0x65,
+	                     0x6d,0x70,0x4d,0x1b,0x61,0x33,0x32,0x63,0x38,0x35,
+	                     0x62,0x61,0x39,0x64,0x64,0x61,0x34,0x35,0x38,0x32,
+	                     0x33,0x62,0x65,0x34,0x31,0x36,0x32,0x34,0x36,0x63,
+	                     0x66,0x38,0x62,0x34,0x33,0x33,0x62,0x61,0x61,0x30,
+	                     0x36,0x38,0x64,0x37,0xFF,0x39,0x39};
+	coap_pdu msg_ref = {ref_bin, 57, 57};
+
+	uint8_t test_bin[57];
+	coap_pdu msg_test = {test_bin, 0, 57};
+
+	mu_assert("[ERROR] Empty CON failed to init.",
+	          coap_init_pdu(&msg_test) == CE_NONE);
+
+	mu_assert("[ERROR] POST CON failed to set version.",
+	          coap_set_version(&msg_test, COAP_V1) >= 0);
+
+	mu_assert("[ERROR] POST CON failed to set type.",
+	          coap_set_type(&msg_test, CT_CON) >= 0);
+
+	mu_assert("[ERROR] POST CON failed to set code.",
+	          coap_set_code(&msg_test, CC_POST) >= 0);
+
+	mu_assert("[ERROR] POST CON failed to set message ID.",
+	          coap_set_mid(&msg_test, 0x37) >= 0);
+
+	mu_assert("[ERROR] POST CON failed to set token.",
+	          coap_set_token(&msg_test, 0, 0) >= 0);
+
+	mu_assert("[ERROR] POST CON failed to add first path option.",
+	          coap_add_option(&msg_test, CON_URI_PATH, ref_bin+5, 2) == CE_NONE);
+
+	mu_assert("[ERROR] POST CON failed to add second path option.",
+	          coap_add_option(&msg_test, CON_URI_PATH, ref_bin+8, 4) == CE_NONE);
+
+	mu_assert("[ERROR] POST CON failed to add query option.",
+	          coap_add_option(&msg_test, CON_URI_QUERY, ref_bin+14, 40) == CE_NONE);
+
+	mu_assert("[ERROR] POST CON failed to add payload.",
+	          coap_set_payload(&msg_test, ref_bin+55, 2) == CE_NONE);
+
+	mu_assert("[ERROR] POST CON length set wrong.",
+	          msg_test.len == 57);
+
+	mu_assert("[ERROR] POST CON failed to encode.",
+	          memcmp(msg_ref.buf, msg_test.buf, 57) == 0);
+
+	return 0;
+}
+
+static char * test_msg_content_ack_getters() {
+	uint8_t ref_bin[] = {0x61,0x45,0xEE,0xCC,0xA2,0xFF,0x35,0x36};
+	coap_pdu msg_ref = {ref_bin, 8, 8};
+
+	coap_payload payload;
+
+	mu_assert("[ERROR] CONTENT ACK failed validation.",
+	          coap_validate_pkt(&msg_ref) == CE_NONE);
+
+	mu_assert("[ERROR] CONTENT ACK version decoded wrong.",
+	          coap_get_version(&msg_ref) == COAP_V1);
+
+	mu_assert("[ERROR] CONTENT ACK type decoded wrong.",
+	          coap_get_type(&msg_ref) == CT_ACK);
+
+	mu_assert("[ERROR] CONTENT ACK code decoded wrong.",
+	          coap_get_code(&msg_ref) == CC_CONTENT);
+
+	mu_assert("[ERROR] CONTENT ACK code class decoded wrong.",
+	          coap_get_code_class(&msg_ref) == 2);
+
+	mu_assert("[ERROR] CONTENT ACK code detail decoded wrong.",
+	          coap_get_code_detail(&msg_ref) == 5);
+
+	payload = coap_get_payload(&msg_ref);
+	mu_assert("[ERROR] CONTENT ACK payload length was wrong.",
+	          payload.len == 2);
+	mu_assert("[ERROR] CONTENT ACK payload value was wrong.",
+	          memcmp(payload.val, ref_bin+6, 2) == 0);
+
+	return 0;
+}
+
+static char * test_msg_pl_from_get_opt_no_opt() {
+	uint8_t ref_bin[] = {0x62, 0x45, 0xee, 0x8b, 0x0e, 0x50, 0xff, 0x53,
+		                 0x68, 0x6f, 0x6f, 0x70};
+	coap_pdu msg_ref = {ref_bin, 12, 12};
+
+	coap_option payload;
+
+	mu_assert("[ERROR] PAYLOAD NO OPT failed validation.",
+	          coap_validate_pkt(&msg_ref) == CE_NONE);
+
+	payload = coap_get_option(&msg_ref, 0);
+	mu_assert("[ERROR] PAYLOAD NO OPT didn't find payload.",
+	          payload.num == 0);
+	mu_assert("[ERROR] PAYLOAD NO OPT payload length was wrong.",
+	          payload.len == 5);
+	mu_assert("[ERROR] PAYLOAD NO OPT payload value was wrong.",
+	          memcmp(payload.val, ref_bin+7, 5) == 0);
+
+	return 0;
+}
+
+// Helpers
+void hex_dump(char* bytes, size_t len)
+{
+  size_t i, j;
+  for (i = 0; i < len; i+=16){
+    printf("  0x%.3zx    ", i);
+    for (j = 0; j < 16; j++){
+      if (i+j < len)
+        printf("%.2hhx ", bytes[i+j]);
+      else
+        printf("%s ", "--");
+    }
+    printf("   %.*s\n", (int)(16 > len-i ? len-i : 16), bytes+i);
+  }
+}
+
+void coap_pretty_print(coap_pdu *pdu)
+{
+  if (coap_validate_pkt(pdu) == 0){
+    printf("Found Valid Coap Packet\n");
+  }
+
+  hex_dump((char*)pdu->buf, pdu->len);
+}
+
+static char * all_tests() {
+	// Make Sure the Tests Are Working
+	mu_run_test(test_math);
+
+	// Actually Run the Real Tests
+	mu_run_test(test_msg_empty_con_getters);
+	mu_run_test(test_msg_empty_con_getters_with_token);
+	mu_run_test(test_msg_empty_con_setters);
+	mu_run_test(test_msg_get_con_getters);
+	mu_run_test(test_msg_get_con_setters);
+	mu_run_test(test_msg_content_ack_getters);
+	mu_run_test(test_msg_get_con_setters_out_order);
+	mu_run_test(test_msg_post_con_setters);
+	mu_run_test(test_msg_pl_from_get_opt_no_opt);
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	char *result = all_tests();
+	if (result != 0) {
+		printf("%s\n", result);
+	}
+	else {
+		printf("ALL TESTS PASSED\n");
+	}
+	printf("Tests run: %d\n", tests_run);
+
+	return result != 0;
+}
+
